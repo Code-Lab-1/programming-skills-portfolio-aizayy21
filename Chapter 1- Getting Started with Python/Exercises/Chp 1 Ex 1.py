cc50a1c83be55a167d6318c6aecaca3491c7b001
aizayy21 <113715817+aizayy21@users.noreply.github.com>
2022-10-30 17:18:59 +0400

    Chp 1 Ex 4

---
+++ Chapter 1- Getting Started with Python/Exercises/Chp 1 Ex 1.py
@@ -0,0 +1,9 @@
+A = """
+Twinkle, twinkle, little star,
+how I wonder what you are!
+up above the world so high,
+like a dimond in the sky.
+twinkle, twinkle, little star,
+how I wonder what you are
+"""
+print(A)
