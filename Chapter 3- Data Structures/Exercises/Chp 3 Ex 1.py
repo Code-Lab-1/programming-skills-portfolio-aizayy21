7b3d65d9fdb02a4a42ce39720ea64ffde50b31ff
aizayy21 <113715817+aizayy21@users.noreply.github.com>
2022-11-01 01:20:57 +0400

    commit

--- Chapter 3- Data Structures/Exercises/Chp 3 Ex 1.py
+++ Chapter 3- Data Structures/Exercises/Chp 3 Ex 1.py
@@ -1,8 +1,9 @@
 from tkinter.font import names
 
 
-names=("Faiza","Sara","Haleema")
+names=("Faiza","Sara","Haleema,","Eram") 
 print(names)
 print(names[0])
 print(names[1])
 print(names[2])
+print(names[3])
