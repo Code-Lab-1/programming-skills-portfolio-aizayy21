d54b884bdfbaaba194861cb8f6c84adaed069dfe
aizayy21 <113715817+aizayy21@users.noreply.github.com>
2022-11-24 19:41:16 +0400

    Commit to main

---
+++ Chapter 4- Control Flow/Exercises/Chp 4 Ex 3.py
@@ -0,0 +1,8 @@
+alein_color = 'purple'
+if alein_color == 'green':
+    print("you have earned some 5 points.")
+elif alein_color == 'Pale Yellow':
+    print("you have earned some 10 points.")
+
+else:
+    print("you have earned some 15 points.")
---
+++ Chapter 5- Dictionaries/Exercises/Chp 5 Ex 3.py
@@ -0,0 +1,12 @@
+glossary =  {
+    'flowchart': 'Graphical Presentation.',
+    'comment': 'It is represented by symbol.',
+    'list': 'It is unique glossary basically a list .',
+    'loop': 'Working on a statement multiple times.',
+    'dictionary': 'value assigned to different variables.'
+
+    'float: decimal value',
+    'boolean expression': 'Expression that evaluates to True or False.',}
+
+for word, definition in glossary.items():
+    print("\n" + word.title() + ": " + definition)
