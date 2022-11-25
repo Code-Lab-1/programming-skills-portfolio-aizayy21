5ec660fe9137aa3518ecb59c0110fbb8f6380efb
aizayy21 <113715817+aizayy21@users.noreply.github.com>
2022-11-25 18:10:36 +0400

    Commit to main

---
+++ Chapter 7- Functions/Exercises/Chp 7 Ex 3.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+    """Customize your own shirt okay."""
+    print("\nI'm going to make a " + size + " t-shirt.")
+    print('Okay, but please write it, "' + message + '"')
+
+make_shirt('large', 'I love my university!')
+make_shirt(message="Bella Vitta.", size='medium')
